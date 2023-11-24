13a9369368e5ad7423e3a3413058401d8a4b333e
Tobias Meyhöfer <s87766@bht-berlin.de>
2023-11-24 13:20:39 +0100

    v0.12

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>TM</h1>
-      <p>V0.11 under construction... release V1 planned on in January </p>
+      <p>under construction</p>
     </main>
   )
 }
