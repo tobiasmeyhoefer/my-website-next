94dd4de01bf3302d6f7269ae3cda5fd80225e8fe
Tobias Meyhöfer <s87766@bht-berlin.de>
2023-11-25 00:25:53 +0100

    easter egg

---
+++ app/easterEgg/page.tsx
@@ -0,0 +1,3 @@
+export default function EasterEgg() {
+  return <h1>Glückwunsch DENNIS</h1>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,41 @@
+"use client"
+
+import Link from 'next/link'
 import styles from './page.module.css'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+
+  const router = useRouter()
+
+  let counter = 0;
+  function countUpperBody() {
+    console.log(counter)
+    if(counter >= 21) {
+      counter = 0;
+    }
+    counter++;
+  }
+
+  function countDownerBuddy() {
+    console.log(counter)
+    if(counter < 20) {
+      counter = 0;
+    }
+
+    if(counter === 33) {
+      router.push("/easterEgg")
+    }
+
+    counter++;
+  }
+
   return (
     <main className={styles.main}>
       <h1>TM</h1>
       <p>under construction</p>
+      <div onClick={countUpperBody} className={styles.upperBody}></div>
+      <div onClick={countDownerBuddy} className={styles.downerBody}></div>
     </main>
   )
 }
