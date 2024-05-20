772ff4424a7c2b62b845f9488c479bbd44f5c633
Tobias Meyhöfer <tobias.meyhoefer02@gmail.com>
2024-05-20 11:59:02 +0200

    fix

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -6,10 +6,10 @@ import ShinyButton from "@/components/ui/shiny-button";
 export default function NavBar() {
   return (
     <nav className="flex h-[140px] justify-center">
-      <div className="flex w-full max-w-[1920px] items-center justify-between md:px-10 px-4">
+      <div className="flex w-full max-w-[1920px] items-center justify-between md:px-10 px-5">
         <div className="flex items-center gap-2 md:gap-12">
           <Link
-            className=" group flex h-16 w-16 items-center justify-center object-cover"
+            className=" group w-8 h-10 flex items-center justify-center object-cover"
             href={"/"}
           >
             <Image className="w-8 group-hover:rotate-180 transition-all" src={Logo} alt="Logo tm"></Image>
