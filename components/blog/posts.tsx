3f4b8addc54522ab6ad07d91eab9894013572075
Tobias Meyhöfer <tobias.meyhoefer02@gmail.com>
2024-10-06 15:19:22 +0200

    fixed a thing

--- components/blog/posts.tsx
+++ components/blog/posts.tsx
@@ -7,7 +7,7 @@ export function Posts({ posts }: { posts: POSTS_QUERYResult }) {
         <li key={post._id}>
           <a
             className="block p-4 hover:bg-blue-50"
-            href={`/posts/${post?.slug?.current}`}
+            href={`/blog/${post?.slug?.current}`}
           >
             {post?.title}
           </a>
