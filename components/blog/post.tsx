e5fef7352811f8f565059a53ce1af7f3d98fad71
Tobias Meyhöfer <tobias.meyhoefer02@gmail.com>
2024-10-14 15:00:28 +0200

    tried to fix some things

--- components/blog/post.tsx
+++ components/blog/post.tsx
@@ -1,25 +1,37 @@
 import { POST_QUERYResult } from "../../sanity.types";
 import Link from "next/link";
 import MdxBody from "./mdx-body";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import { useEffect } from "react";
 
 function portableTextToPlain(blocks: any[] = []): string {
   return blocks
-    .map(block => {
-      if (block._type !== 'block' || !block.children) {
-        return ''
+    .map((block) => {
+      if (block._type !== "block" || !block.children) {
+        return "";
       }
-      return block.children.map((child: any) => child.text).join('')
+      return block.children.map((child: any) => child.text).join("");
     })
-    .join('\n\n')
+    .join("\n\n");
 }
 export function Post({ post }: { post: POST_QUERYResult }) {
   const { title, mainImage, body } = post || {};
-  const plainBody = body ? portableTextToPlain(body) : ''
+  const plainBody = body ? portableTextToPlain(body) : "";
 
   return (
     <div className="p-4">
-      {title ? <h1 className="mt-10 md:mb-12 mb-6 md:text-5xl text-3xl font-bold">{title}</h1> : null}
+      {title ? (
+        <h1 className="mb-6 mt-10 text-3xl font-bold md:mb-12 md:text-5xl">
+          {title}
+        </h1>
+      ) : null}
       <MdxBody body={plainBody}/>
+      {/* <MDXRemote
+        source={`# Hello World
+
+      This is from Server Components!
+      `}
+      /> */}
       <hr />
       <Link href="/">&larr; Return home</Link>
     </div>
--- next.config.js
+++ next.config.js
@@ -5,13 +5,9 @@ const withMDX = require("@next/mdx")();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "assets.aceternity.com",
-      },
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
