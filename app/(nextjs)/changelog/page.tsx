d11e8d0af2df9484bdd54fee7db550991a46556e
Tobias Meyhöfer <tobias.meyhoefer02@gmail.com>
2024-11-13 15:05:11 +0100

    adjusted some stuff

--- app/(nextjs)/changelog/page.tsx
+++ app/(nextjs)/changelog/page.tsx
@@ -37,7 +37,7 @@ export default function CareerPage() {
             international companies with Next.js and Sanity
           </p>
           <div className="grid grid-cols-2 gap-4">
-            <Image
+            {/* <Image
               src="/images/peaks_trusted.png"
               alt="Peaks & Pies trusted"
               width={500}
@@ -50,7 +50,7 @@ export default function CareerPage() {
               width={500}
               height={500}
               className="aspect-video w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
+            /> */}
           </div>
         </div>
       ),
@@ -64,7 +64,7 @@ export default function CareerPage() {
             knowledge
           </p>
           <div className="grid grid-cols-2 gap-4">
-            <Image
+            {/* <Image
               src="/images/mann.jpg"
               alt="Man smiling"
               width={500}
@@ -77,7 +77,7 @@ export default function CareerPage() {
               width={500}
               height={500}
               className="aspect-video w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
+            /> */}
           </div>
         </div>
       ),
@@ -91,7 +91,7 @@ export default function CareerPage() {
             web applications, to relieve the daily works of more than 15000
             coworkers
           </p>
-          <div className="grid grid-cols-2 gap-4">
+          {/* <div className="grid grid-cols-2 gap-4">
             <Image
               src="/images/work1.jpg"
               alt="BVG work environment"
@@ -106,7 +106,7 @@ export default function CareerPage() {
               height={500}
               className="aspect-video w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
-          </div>
+          </div> */}
         </div>
       ),
     },
--- components/ui/background-beams-with-collision.tsx
+++ components/ui/background-beams-with-collision.tsx
@@ -199,7 +199,7 @@ const CollisionMechanism = React.forwardRef<
           repeatDelay: beamOptions.repeatDelay || 0,
         }}
         className={cn(
-          "absolute left-0 top-20 m-auto h-14 w-px rounded-full bg-gradient-to-t from-emerald-500 via-emerald-800 to-transparent",
+          "absolute left-0 top-20 m-auto h-14 w-px rounded-full bg-gradient-to-t from-zinc-500 via-zinc-800 to-transparent",
           beamOptions.className
         )}
       />
@@ -238,7 +238,7 @@ const Explosion = ({ ...props }: React.HTMLProps<HTMLDivElement>) => {
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 1.5, ease: "easeOut" }}
-        className="absolute -inset-x-10 top-0 m-auto h-2 w-10 rounded-full bg-gradient-to-r from-transparent via-emerald-800 to-transparent blur-sm"
+        className="absolute -inset-x-10 top-0 m-auto h-2 w-10 rounded-full bg-gradient-to-r from-transparent via-zinc-800 to-transparent blur-sm"
       ></motion.div>
       {spans.map((span) => (
         <motion.span
@@ -250,7 +250,7 @@ const Explosion = ({ ...props }: React.HTMLProps<HTMLDivElement>) => {
             opacity: 0,
           }}
           transition={{ duration: Math.random() * 1.5 + 0.5, ease: "easeOut" }}
-          className="absolute h-1 w-1 rounded-full bg-gradient-to-b from-emerald-800 to-emerald-500"
+          className="absolute h-1 w-1 rounded-full bg-gradient-to-b from-zinc-800 to-zinc-500"
         />
       ))}
     </div>
